37fcbee9ffff584e1ad84f71c00b8949bf3ac876
brendenozie <brendenodhiambo@gmail.com>
2017-01-17 23:56:26 +0300

    fifth commit

--- primenumbers/primenumbers.py
+++ primenumbers/primenumbers.py
@@ -1,60 +1,93 @@
 import unittest
 
-def primeNumberFunction(x): 
-    return x
-
-print(primeNumberFunction(5))
+def primeNumberFunction(input_value):
+    while True:
+        try:
+            primeNumber=[]
+            for i in range(input_value+1):
+                li=[]
+                if i>1:
+                    for num in range(1,input_value):
+                        if i%num==0:
+                            li.append(num)
+                            if len(li)==2:
+                                if li[1]==i:
+                                    primeNumber.append(i)
+                                else:
+                                    pass
+            return primeNumber
+        except (TypeError):
+            return "The function only expects a number"
+        except (AssertionError):
+            return "The function only expects a number"
+        except (AttributeError):
+            return "The function only expects a number" 
 
 class MyTestCases(unittest.TestCase):
     def testOne(self):
-        self.assertEqual(primeNumberFunction("A"), "The function only expects a number")
+        result=primeNumberFunction(10)
+        print("prime numbers include", result)
+        self.assertEqual(result,[2,3,5,7] ,msg="The function only expects a number")
     def testTwo(self):
-        self.assertEqual(primeNumberFunction(20), [1,2,3,5,7,11,13,17])
+        result=primeNumberFunction(20)
+        print("prime numbers include", result)
+        self.assertEqual(result, [2,3,5,7,11,13,17,19],msg="The function only expects a number")
     def testThree(self):
-        self.assertEqual(primeNumberFunction(-5), "There are no negative pime numbers")
+        result=primeNumberFunction(100)
+        print("prime numbers include", result)
+        self.assertEqual(result,[2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97] ,msg="The function only expects a number")
     def testFour(self):
-        self.assertEqual(primeNumberFunction(0), "Not a prime number")
+        result=primeNumberFunction(4)
+        print("prime numbers include", result)
+        self.assertEqual(result,[2,3] ,msg="Not a prime number")
     def testFive(self):
-        self.assertEqual(primeNumberFunction(1), 1)
+        result=primeNumberFunction(3)
+        self.assertEqual(result, [2],msg="The function only expects a number")
     def testSix(self):
-        self.assertEqual(primeNumberFunction([1,2,3]), "Not a number")
+        result=primeNumberFunction(7)
+        print("prime numbers include", result)
+        self.assertEqual(result,[2,3,5],msg="The function only expects a number")
     def testSeven(self):
-        self.assertEqual(primeNumberFunction(5), [1,2,3,5])
+        result= primeNumberFunction(5)
+        print("prime numbers include", result)
+        self.assertEqual(result,[2,3],msg="The function only expects a number")
     def testEight(self):
-        self.assertEqual(primeNumberFunction(1.3), "Only accept integers")
+        result=primeNumberFunction(9)
+        print("prime numbers include", result)
+        self.assertEqual(result,[2,3,5,7] ,msg="The function only expects a number")
     def testNine(self):
-        self.assertEqual(primeNumberFunction(0.7), "Only accept integers")
+        result=primeNumberFunction(25)
+        print("prime numbers include", result)
+        self.assertEqual(result,[2,3,5,7,11,13,17,19,23], msg="The function only expects a number")
     def testTen(self):
-        stuff = {'string': 'name', 'int': 25}
-        self.assertEqual(primeNumberFunction(stuff), "Only accept integers")
+        result=primeNumberFunction(13)
+        print("prime numbers include", result)
+        self.assertEqual(result,[2,3,5,7,11],msg="The function only expects a number")
 
 if __name__ == '__main__':
     unittest.main()
 
       
-def primeNumberFunction(input_value):
-  try:
-    primeNumber=[]
-    for i in range(input_value+1):
-      li=[]
-      if i>1:
-        for num in range(1,input_value):
-          if i%num==0:
-            li.append(num)
-            if len(li)==2:
-              if li[1]==i:
-                primeNumber.append(i)
-              else:
-                pass
-      
-    return primeNumber 
-  
-  except (TypeError):
-    raise ValueError('')
-  except (AttributeError):
-    raise ValueError('The provided input is not a dictionary')
-  except (AttributeError):
-    raise ValueError('The provided input is not a dictionary')
+
     
-l=primeNumberFunction(1.3)
-print l
+  
+    
+  
+    
+                
+      
+    
+                
+              
+                
+              
+            
+            
+          
+        
+      
+     
+    
+    
+    
+
